cf4f8497e48e4bb0c4dafc3a5c5a91f49224bfc3
willbrooks37 <78041850+willbrooks37@users.noreply.github.com>
2021-03-03 21:19:10 -0600

    changes that dont work

--- marketing.py
+++ marketing.py
@@ -1,9 +1,19 @@
 import re
+from collections import Counter 
 
 log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
 
-pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d .*)'
+pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)'
 
+Regexp ={}
 
+for line in open('logfile.log'):
+    pieces = re.split(pattern, line)
+    
+date = pieces[4]
+if date in Regexp:
+    Regexp[date] += 1
+else:
+    Regexp[date] = 1
 
-
+print (Regexp)
--- requests.py
+++ requests.py
@@ -5,14 +5,8 @@ log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200
 
 pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)'
 
-Regexp ={}
+pieces = []
 
 for line in open('logfile.log'):
-    pieces = re.split(pattern, line)
-    
-request = pieces[4]
-def most_frequent(pieces):
-    occurence_count = Counter(pieces)
-    return occurence_count.most_common(4)[0][0]
-print(most_frequent(pieces))
-print (Regexp)
+    pieces.append(re.split(pattern, line))
+
