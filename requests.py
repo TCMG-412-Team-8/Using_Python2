f4d127cbeaeb635487fcf77f4a916126057dc980
willbrooks37 <78041850+willbrooks37@users.noreply.github.com>
2021-03-03 18:14:38 -0600

    Update requests.py

--- requests.py
+++ requests.py
@@ -2,5 +2,5 @@ import re
 
 things = {}
 for line in open('logfile.log'):
-    pieces = re.split("\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)"gm, line)
+    pieces = re.split(r"\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)", line)
     #I am so damn confused. I take nap. I be back at 5pm.
