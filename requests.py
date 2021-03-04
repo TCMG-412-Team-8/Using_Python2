9abdf744df942bfd598bcdf8b1816e25a4279e76
willbrooks37 <78041850+willbrooks37@users.noreply.github.com>
2021-03-03 21:55:57 -0600

    progress?

--- requests.py
+++ requests.py
@@ -3,10 +3,19 @@ from collections import Counter
 
 log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
 
-pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)'
+pattern= r'\s[0-z]+.[a-z]+'
 
-pieces = []
+Regexp ={}
 
 for line in open('logfile.log'):
-    pieces.append(re.split(pattern, line))
+    match = re.search(pattern, line)
+    try:
+        date = match.groups()
+    except AttributeError:
+        date = match
+    if date in Regexp:
+        Regexp[date] += 1
+    else:
+        Regexp[date] = 1
 
+print (Regexp)
