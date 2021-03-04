d6fab5482d007e43c3215c5bf41ec7377d839654
Bolotz <78045803+Bolotz@users.noreply.github.com>
2021-03-04 13:28:52 -0600

    Delete marketing.py

--- marketing.py
+++
@@ -1,9 +0,0 @@
-import re
-
-log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
-
-pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d .*)'
-
-
-
-
