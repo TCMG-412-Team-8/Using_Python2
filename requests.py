0ff0c0f4db0f1c8c2d6338b4b949d3196ec1c746
willbrooks37 <78041850+willbrooks37@users.noreply.github.com>
2021-03-03 18:47:24 -0600

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,6 +1,18 @@
 import re
+from collections import Counter 
 
-things = {}
+log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
+
+pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)'
+
+Regexp ={}
+
 for line in open('logfile.log'):
-    pieces = re.split(r"\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)", line)
-    #I am so damn confused. I take nap. I be back at 5pm.
+    pieces = re.split(pattern, line)
+    
+request = pieces[4]
+def most_frequent(pieces):
+    occurence_count = Counter(pieces)
+    return occurence_count.most_common(4)[0][0]
+print(most_frequent(pieces))
+print (Regexp)
