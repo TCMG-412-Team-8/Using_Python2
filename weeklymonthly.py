e9efbf4063a59ce847ce531ef3f742b797c6e7be
nromano263 <78044734+nromano263@users.noreply.github.com>
2021-03-03 20:12:38 -0600

    Add files via upload

---
+++ weeklymonthly.py
@@ -0,0 +1,35 @@
+import re
+from collections import counter
+
+log_line = 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
+
+pattern= r'\[(]^:]*)'
+month_pattern = r'[A-Z][a-z][a-z]'
+
+Regexp ={}
+
+for line in open('logfile.log')
+    match = re.search(pattern, line)
+    try:
+        date = match.groups()
+    except AttributeError:
+        date = match
+    if date in Regexp:
+        Regexp[date] += 7
+    else:
+        Regexp[date] = 7
+    
+for line in open('logfile.log')
+    month = re.search(month_pattern, line)
+    try:
+        date = month.groups()
+    except AttributeError
+        date = match
+    if date in Regexp:
+        
+        
+
+print(Regexp)
+
+
+
